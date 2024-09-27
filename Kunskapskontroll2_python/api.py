b7a812e2e6682565e3ebee3c21634cb161a0b709
Keikiet <134530916+Keikiet@users.noreply.github.com>
2024-09-27 11:13:17 +0200

    Delete Kunskapskontroll2_python directory

--- Kunskapskontroll2_python/api.py
+++
@@ -1,9 +0,0 @@
-# API settings
-
-API_KEY = 'Q04MKY2N7VG5XJ2A'
-
-SYMBOLS = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'ASML',
-           'TSLA', 'NVDA', 'ADBE', 'BTC', 'NFLX',
-           'PEP', 'KO', 'V', 'MA'
-          ] 
-
